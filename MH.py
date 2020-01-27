37485d2e835c384c0a274ac46a7ab88f14550f6b
mh458 <60277513+mh458@users.noreply.github.com>
2020-01-27 19:55:50 +0200

    Create MH.py

---
+++ MH.py
@@ -0,0 +1,309 @@
+from os import *
+from sys import *
+from time import *
+
+#.           banner h
+def mh () :
+    system ("sleep 0.3")
+    print ("\033[0;31m      ___  ___ \033[0;32m  _   _ ") 
+    system ("sleep 0.3")
+    print ("\033[0;31m     /   |/   |\033[0;32m | | | |") 
+    system ("sleep 0.3")
+    print ("\033[0;31m    / /|   /| |\033[0;32m | |_| |") 
+    system ("sleep 0.3")
+    print ("\033[0;31m   / / |__/ | |\033[0;32m |  _  |") 
+    system ("sleep 0.3")
+    print ("\033[0;31m  / /       | |\033[0;32m | | | |") 
+    system ("sleep 0.3")
+    print ("\033[0;31m /_/        |_|\033[0;32m |_| |_|")
+mh ()
+
+#.            color
+#y \033[0;33m
+#r \033[0;31m
+#g \033[0;32m
+#b \033[0;34m
+#.            codes
+system ("sleep 0.3")
+print ("\033[0;31m**********\033[0;31mwelcome to MH Tool\033[0;32m**********")
+system ("sleep 0.3")
+print ("                                                          ")
+print ("\033[0;34m**********\033[0;31mTools\033[0;32m**********")
+system ("sleep 0.3")
+print ("\033[0;32m 1-\033[0;33mA-Rat")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 2-\033[0;33mLazymux")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+print ("\033[0;32m 3-\033[0;33mTool-X")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 4-\033[0;33mroutersploit")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 5-\033[0;33mngrok")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 6-\033[0;33mmetasploi")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 7-\033[0;33mTmux-Bunch")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 8-\033[0;33msite for making 1fake sites ")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 9-\033[0;33minstall Kali Linux")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 10-\033[0;33mreport")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 11-\033[0;33mtermux basics")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+print ("\033[0;32m 12-\033[0;33mexit")
+system ("sleep 0.3")
+print ("\033[0;34m++++++++++++++++++++++")
+system ("sleep 0.3")
+mh =input ("\033[0;31mchose number: ")
+if mh =="1" : 
+    system ("clear")
+    system ("apt upgrade & apt update ")
+    system ("apt install git")
+    system ("apt install python ")
+    system ("apt install python2")
+    system ("git clone https://github.com/Xi4u7/A-Rat")
+    system ("sleep 0.5")
+    print ("thanks for using MH Tool")
+    print ("12-\033[0;33mexit")
+    print ("13-\033[0;33mback")
+    mp =input ("write number: ")
+    if mp =="12" :
+        exit ()
+    if mp =="13" :
+        system ("Python mh.py")
+if mh =="2" :
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("apt install git ")
+    system ("apt install python")
+    system ("git clone https://github.com/Gameye98/Lazymux ")
+    system ("sleep 0.5")
+    print ("thanks for using MH Tool")
+    print ("11-\033[0;33mexit")
+    print ("12-\033[0;33mback")
+    mo =input ("write number: ")
+    if mo =="12" :
+        exit ()
+    if mo =="13" :
+        system ("Python mh.py")
+    
+
+if mh =="3" :
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("apt install git ")
+    system ("sleep 0.5")
+    print ("thanks for using MH Tool")
+    print ("11-\033[0;33mexit")
+    print ("12-\033[0;33mback")
+    ml =input ("write number: ")
+    if ml =="12" :
+        exit ()
+    if ml =="13" :
+        system ("Python mh.py")
+
+
+if mh =="4" :
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("apt install python")
+    system ("apt install python2")
+    system ("git clone https://github.com/reverse-shell/routersploit.git")
+    system ("sleep 0.5")
+    print ("thanks for using MH Tool")
+    print ("13-\033[0;33mexit")
+    print ("13-\033[0;33mback")
+    mj =input ("write number: ")
+    if mj =="12" :
+        exit ()
+    if mj =="13" :
+        system ("Python mh.py")  
+        
+if mh =="5" :
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("apt install git")
+    system ("git clone https://github.com/inconshreveable/ngrok")
+    system ("sleep 0.5")
+    print ("thanks for using MH Tool")
+    print ("13-\033[0;33mexit")
+    print ("13-\033[0;33mback")
+    mn =input ("write number: ")
+    if mn =="12" :
+        exit ()
+    if mn =="13" :
+        system ("Python mh.py")    
+    
+    
+if mh =="5" :   
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("git clone https://github.com/inconshreveable/ngrok")      
+    print ("thanks for using MH Tool")
+    print ("11-exit")
+    print ("12-back")
+    mc =input ("write number: ")
+    if mc =="12" :
+        exit ()
+    if mc =="13" :
+        system ("Python mh.py")
+if mh =="6" :
+    system ("clear")
+    system ("apt update && apt upgrade -y")
+    system ("pkg install unstable-repo -y")
+    system ("pkg install metasploit -y ")
+    print ("thanks for using MH Tool")
+    print ("11-exit")
+    print ("12-back")
+    ma =input ("write number: ")
+    if ma =="12" :
+        exit ()
+    if ma =="13" :
+        system ("Python mh.py")
+        
+if mh =="7" :    
+    system ("clear")
+    system ("apt update & apt upgrade")     
+    system ("pkg install git ")
+    system ("git clone https://github.com/Hax4us/Tmux-Bunch")
+    print ("thanks for using MH Tool")
+    print ("12-exit")
+    print ("13-back")
+    mu =input ("write number: ")
+    if mu =="13" :
+        exit ()
+    if mu =="13" :
+        system ("Python mh.py") 
+
+if mh =="8" :
+    system ("clear")
+    system ("termux-open http://sp0m.com/?method=logout")
+    print ("thanks for using MH Tool")
+    print ("12-exit")
+    print ("13-back")
+    me =input ("write number: ")
+    if me =="12" :
+        exit ()
+    if me =="13" :
+        system ("Python mh.py")
+        
+if mh =="9" :
+    system ("clear")
+    system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh")
+    print ("thanks for using MH Tool")
+    print ("12-exit")
+    print ("13-back")
+    ms =input ("write number: ")
+    if ms =="12" :
+        exit ()
+    if ms =="13" :
+        system ("Python mh.py")    
+    
+if mh =="10" :
+    system ("clear")
+    system ("apt update & apt upgrade")
+    system ("pkg install git")
+    system ("git clone https://github.com/IlayTamvan/Report")
+    print ("thanks for using MH Tool")
+    print ("12-exit")
+    print ("13-back")
+    ms =input ("write number: ")
+    if ms =="12" :
+        exit ()
+    if ms =="13" :
+        system ("Python mh.py")
+        
+        
+if mh =="11" : 
+    system ("clear")
+    system ("    pkg update -y")
+    system ("    pkg upgrade -y")
+    system ("    pkg install python -y")
+    system ("    pkg install python2 -y")
+    system ("    pkg install python2-dev -y")
+    system ("    pkg install python3 -y")
+    system ("    pkg install java -y")
+    system ("    pkg install fish -y")
+    system ("    pkg install ruby -y")
+    system ("    pkg install help -y")
+    system ("    pkg install git -y")
+    system ("    pkg install host -y")
+    system ("    pkg install php -y")
+    system ("    pkg install perl -y")
+    system ("    pkg install nmap -y")
+    system ("    pkg install bash -y")
+    system ("    pkg install clang -y")
+    system ("    pkg install nano -y")
+    system ("    pkg install w3m -y")
+    system ("    pkg install havij -y")
+    system ("    pkg install hydra -y")
+    system ("    pkg install figlet -y")
+    system ("    pkg install cowsay -y")
+    system ("    pkg install curl -y")
+    system ("    pkg install tar -y")
+    system ("    pkg install zip -y")
+    system ("    pkg install unzip -y")
+    system ("    pkg install tor -y")
+    system ("   pkg install google -y")
+    system ("    pkg install sudo -y")
+    system ("    pkg install wget -y")
+    system ("    pkg install wireshark -y")
+    system ("    pkg install wgetrc -y")
+    system ("    pkg install wcalc -y")
+    system ("    pkg install bmon -y")
+    system ("    pkg install vpn -y")
+    system ("    pkg install unrar -y")
+    system ("    pkg install toilet -y")
+    system ("    pkg install proot -y")
+    system ("    pkg install net-tools -y")
+    system ("    pkg install golang -y")
+    system ("    pkg install chroot -y")
+    system ("    termux-chroot -y")
+    system ("    pkg install macchanger -y")
+    system ("    pkg install openssl -y")
+    system ("    pkg install cmatrix -y")
+    system ("    pkg install openssh -y")
+    system ("    pkg install wireshark -y")
+    system ("    termux-setup-storage -y")
+    system ("    pkg install macchanger -y")
+    system ("    pkg install python3")
+    print ("thanks for using MH Tool")
+    print ("12-exit")
+    print ("13-back")
+    md =input ("write number: ")
+    if md =="12" :
+        exit ()
+    if md =="13" :
+        system ("Python mh.py")  
+        
+if mh =="12" :  
+    system ("clear")
+    exit ()
+    print ("thanks for using MH Tool")
+    
+    
+           
+ 
